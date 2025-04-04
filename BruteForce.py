c79c4c291c1cc120f2f35ebeb1efecee6db2c403
Wexzup <wexzup@gmail.com>
2025-04-04 18:21:54 +1100

    Update BruteForce.py

--- BruteForce.py
+++ BruteForce.py
@@ -2,12 +2,39 @@ import itertools
 import time
 import subprocess
 
+
 lengthA = int(input("What is the length of the password? : "))
-Characters = input("What is the characters used for the password? : ")
+Mode = input("What is mode for the characters (1,2,3)")
+if int(Mode) == 1 :
+    Characters = input("What is the characters used for the password? : ")
+elif int(Mode) == 2 :
+    print("a,A,1,aA,A1,a1,aA1")
+    WCharacters = input("What is the characters used for the password? : ")
+    if int(WCharacters) == "a" :
+        Characters = "abcdefghijklmnopqrstuvwxyz"
+    elif int(WCharacters) == "A" :
+        Characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    elif int(WCharacters) == 1 :
+        Characters = "0123456789"
+    elif int(WCharacters) == "aA" :
+        Characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    elif int(WCharacters) == "A1" :
+        Characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    elif int(WCharacters) == "a1" :
+        Characters = "abcdefghijklmnopqrstuvwxyz0123456789"
+    elif int(WCharacters) == "aA1" :
+        Characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    else :
+        print("Wrong Answer")
+        UU = input("Press for continue ... ")
+        print(UU)
+elif int(Mode) == 3:
+    Characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+else : 
+    print("Wrong Mode")
+
 ip = input("The ip of your computer : ")
 user = input("What is your user : ")
-
-
 
 def brute_force(chars, length):
     start_time = time.time()
